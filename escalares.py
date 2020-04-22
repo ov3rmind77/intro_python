d18e029e4c8b1a18e7cc69d018e0d3c980d0d805
ov3rmind77 <57711981+ov3rmind77@users.noreply.github.com>
2020-04-22 10:34:56 -0300

    Add files via upload

---
+++ escalares.py
@@ -0,0 +1,18 @@
+# Exemplos com dados escalares.
+# Na lingugem python todos os tipos de dados são tratados como objetos.
+# Assim a programação orientada a objetos é praticada na íntegra.
+# Definição/dimensionamento de objetos do tipo inteiro.
+a = 10
+b = 27
+# Definição/dimensionamento de obejeto do tipo string.
+mensagem = 'Olá, neste exemplo temos as variáveis a, b com os respectivos valores:\n'
+print (mensagem)
+print (a, b, '\n')
+# Atribuição de novo valor pra o objeto string.
+mensagem ='Agora relizaremos uma operação de adição com as variáveis.\n'
+c = a + b
+# Nova atribuição de valor para o objeto string.
+mensagem = 'o resultado da adição é:'
+print (mensagem, c,'\n')
+# Podemos com os tipos de dados elementares de python, com variáveis escalares
+# representar vários tipos de dados numéricos.
