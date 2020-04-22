e728dd876f24ec5f290506571c213bddb7eb6162
Léo Lima <feraleomg@gmail.com>
2020-04-22 12:35:42 -0300

    revisão 0.0a

--- escalares.py
+++ escalares.py
@@ -5,7 +5,7 @@
 a = 10
 b = 27
 # Definição/dimensionamento de obejeto do tipo string.
-mensagem = 'Olá, neste exemplo temos as variáveis a, b com os respectivos valores:\n'
+mensagem = 'Neste exemplo temos as variáveis a, b do tipo inteiro com os respectivos valores:\n'
 print (mensagem)
 print (a, b, '\n')
 # Atribuição de novo valor pra o objeto string.
@@ -16,3 +16,13 @@ mensagem = 'o resultado da adição é:'
 print (mensagem, c,'\n')
 # Podemos com os tipos de dados elementares de python, com variáveis escalares
 # representar vários tipos de dados numéricos.
+# o tipo de dados float, e usado para representação de valores de ponto
+# flutuante e resolução de 64 bits.
+a = 10.74
+b = 26.26
+mensagem = 'Agora temos as variáveis a,b com valores de ponto flutuante(float):'
+print(mensagem)
+print(a, b, '\n')
+c = a + b
+mensagem = 'Agora realizando a operação de adição com valores do tipo float:\n'
+print(mensagem,c)
